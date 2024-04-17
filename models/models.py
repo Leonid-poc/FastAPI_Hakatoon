3fed3bb59bb89d64780ece5a333d077dbbd71ebe
Leonid-poc <legeorg2004@gmail.com>
2024-04-18 03:25:52 +0500

    edit role and user tables, added default columns for FastAPIUser

--- models/models.py
+++ models/models.py
@@ -1,25 +1,30 @@
 from datetime import datetime
-from sqlalchemy import MetaData, Integer, String, ForeignKey, TIMESTAMP, Table, Column, VARCHAR, JSON
+from sqlalchemy import MetaData, Boolean, Integer, \
+    String, ForeignKey, TIMESTAMP, Table, Column, VARCHAR, JSON
 
 metadata = MetaData()
 
 
-roles = Table(
-    'roles',
+role = Table(
+    'role',
     metadata,
     Column('id', Integer, primary_key=True, autoincrement=True),
     Column('title', VARCHAR(255), nullable=False),
     Column('permissions', JSON),
 )
 
-users = Table(
-    'users',
+user = Table(
+    'user',
     metadata,
     Column('id', Integer, primary_key=True, autoincrement=True),
     Column('name', VARCHAR(255), nullable=False),
-    Column('email', VARCHAR(255), nullable=False),
     Column('phone', VARCHAR(50), nullable=True),
-    Column('password', String, nullable=False),
     Column('registered_at', TIMESTAMP, default=datetime.utcnow),
-    Column('role_id', Integer, ForeignKey('roles.id'))
+    Column('role_id', Integer, ForeignKey(role.c.id), default=1),
+
+    Column('email', VARCHAR(255), nullable=False),
+    Column('hashed_password', String, nullable=False),
+    Column('is_active', Boolean, default=True, nullable=False),
+    Column('is_superuser', Boolean, default=False, nullable=False),
+    Column('is_verified', Boolean, default=False, nullable=False)
 )
