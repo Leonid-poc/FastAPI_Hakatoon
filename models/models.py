349f8205d11c03d839093aa1474fe8443b0dc165
Leonid-poc <legeorg2004@gmail.com>
2024-04-18 01:04:38 +0500

    adding model for 2 tables in DB

---
+++ models/models.py
@@ -0,0 +1,24 @@
+from datetime import datetime
+from sqlalchemy import MetaData, Integer, String, ForeignKey, TIMESTAMP, Table, Column, VARCHAR, JSON
+
+metadata = MetaData()
+
+
+roles = Table(
+    'roles',
+    metadata,
+    Column('id', Integer, primary_key=True, autoincrement=True),
+    Column('title', VARCHAR(255), nullable=False),
+    Column('permissions', JSON),
+)
+
+users = Table(
+    'users',
+    metadata,
+    Column('id', Integer, primary_key=True, autoincrement=True),
+    Column('name', VARCHAR(255), nullable=False),
+    Column('email', VARCHAR(255), nullable=False),
+    Column('password', String, nullable=False),
+    Column('registered_at', TIMESTAMP, default=datetime.utcnow),
+    Column('role_id', Integer, ForeignKey('roles.id'))
+)
