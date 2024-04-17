ce20340d37fa1d76b450ce02eeb95824f84efef8
Leonid-poc <legeorg2004@gmail.com>
2024-04-17 23:19:01 +0500

    first commit

---
+++ main.py
@@ -0,0 +1,17 @@
+from pydantic import BaseModel
+from typing import List, Optional
+import fastapi
+
+q = List[str]
+
+class User(BaseModel):
+    name: str
+    age: int
+    email: Optional[str]
+
+app = fastapi.FastAPI(title="Testing")
+
+@app.post('/')
+def testing(user: User):
+    return f'name - {user.name}, age - {user.age}, email - {user.email}'
+
